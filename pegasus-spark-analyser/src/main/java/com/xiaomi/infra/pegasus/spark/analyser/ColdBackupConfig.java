c96f9a5217c980e108110207fab3c9b5dcbe8721
JiaShuo <js982986555@live.com>
2020-04-07 14:37:54 +0800

    format and add different restore version

--- pegasus-spark-analyser/src/main/java/com/xiaomi/infra/pegasus/spark/analyser/ColdBackupConfig.java
+++ pegasus-spark-analyser/src/main/java/com/xiaomi/infra/pegasus/spark/analyser/ColdBackupConfig.java
@@ -7,6 +7,7 @@ public class ColdBackupConfig extends Config {
   public String policyName = "every_day";
   // creating time of cold backup data, for example: 2019-09-11
   public String coldBackupTime = "";
+  // different kv store has different restore version, default set "RecordRestoreV1"
   public RecordRestore recordRestore = new RecordRestoreV1();
 
   public Config setPolicyName(String policyName) {
