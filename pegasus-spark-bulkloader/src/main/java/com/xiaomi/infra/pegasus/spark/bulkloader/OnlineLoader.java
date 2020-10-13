38ad562fe2190e22d93540d25e3859711f63ef16
JiaShuo <js982986555@live.com>
2020-10-13 12:15:41 +0800

    init

--- pegasus-spark-bulkloader/src/main/java/com/xiaomi/infra/pegasus/spark/bulkloader/OnlineLoader.java
+++ pegasus-spark-bulkloader/src/main/java/com/xiaomi/infra/pegasus/spark/bulkloader/OnlineLoader.java
@@ -1,3 +1,55 @@
 package com.xiaomi.infra.pegasus.spark.bulkloader;
 
-public class OnlineLoader {}
+import com.xiaomi.infra.pegasus.client.PException;
+import com.xiaomi.infra.pegasus.client.PegasusClientFactory;
+import com.xiaomi.infra.pegasus.client.PegasusClientInterface;
+import com.xiaomi.infra.pegasus.client.SetItem;
+import com.xiaomi.infra.pegasus.spark.FlowController;
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class OnlineLoader {
+  private static final Log LOG = LogFactory.getLog(OnlineLoader.class);
+
+  private PegasusClientInterface client;
+  private FlowController flowController;
+  private OnlineLoaderConfig onlineLoaderConfig;
+
+  public OnlineLoader(OnlineLoaderConfig config, int partitionTaskCount) throws PException {
+    this.onlineLoaderConfig = config;
+    this.client = PegasusClientFactory.getSingletonClient(config.getClientOptions());
+    if (config.getRateLimiterConfig() != null) {
+      long qps = config.getRateLimiterConfig().getQps();
+      long megabytes = config.getRateLimiterConfig().getMegabytes();
+      double factor = config.getRateLimiterConfig().getBurstFactor();
+
+      this.flowController =
+          new FlowController(partitionTaskCount, factor)
+              .withMBytesLimiter(megabytes)
+              .withQPSLimiter(qps);
+    }
+  }
+
+  public void load(List<SetItem> setItems) throws InterruptedException {
+    boolean success = false;
+    int failedCount = 0;
+    while (!success) {
+      try {
+        if (flowController != null) {
+          int bytes = 0;
+          for (SetItem setItem : setItems) {
+            bytes += setItem.hashKey.length + setItem.sortKey.length + setItem.value.length;
+          }
+          flowController.acquireQPS();
+          flowController.acquireBytes(bytes);
+        }
+        client.batchSet(onlineLoaderConfig.getTableName(), setItems);
+        success = true;
+      } catch (PException e) {
+        LOG.info("batchSet error(" + failedCount + "):" + e);
+        Thread.sleep(100);
+      }
+    }
+  }
+}
--- pegasus-spark-bulkloader/src/main/java/com/xiaomi/infra/pegasus/spark/bulkloader/OnlineLoaderConfig.java
+++ pegasus-spark-bulkloader/src/main/java/com/xiaomi/infra/pegasus/spark/bulkloader/OnlineLoaderConfig.java
@@ -1,3 +1,48 @@
 package com.xiaomi.infra.pegasus.spark.bulkloader;
 
-public class OnlineLoaderConfig {}
+import com.xiaomi.infra.pegasus.client.ClientOptions;
+import com.xiaomi.infra.pegasus.spark.FlowController.RateLimiterConfig;
+
+public class OnlineLoaderConfig {
+  private ClientOptions clientOptions;
+  private RateLimiterConfig rateLimiterConfig;
+  private int ttlThreshold;
+  private String clusterName;
+  private String tableName;
+
+  public OnlineLoaderConfig(ClientOptions clientOptions, String clusterName, String tableName) {
+    this.clientOptions = clientOptions;
+    this.clusterName = clusterName;
+    this.tableName = tableName;
+  }
+
+  public OnlineLoaderConfig setRateLimiterConfig(RateLimiterConfig rateLimiterConfig) {
+    this.rateLimiterConfig = rateLimiterConfig;
+    return this;
+  }
+
+  public OnlineLoaderConfig setTtlThreshold(int ttlThreshold) {
+    this.ttlThreshold = ttlThreshold;
+    return this;
+  }
+
+  public ClientOptions getClientOptions() {
+    return clientOptions;
+  }
+
+  public RateLimiterConfig getRateLimiterConfig() {
+    return rateLimiterConfig;
+  }
+
+  public int getTtlThreshold() {
+    return ttlThreshold;
+  }
+
+  public String getClusterName() {
+    return clusterName;
+  }
+
+  public String getTableName() {
+    return tableName;
+  }
+}
